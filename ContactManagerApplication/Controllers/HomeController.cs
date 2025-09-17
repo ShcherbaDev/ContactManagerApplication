b4a3f62188cf01844aee9643890d00e250f227b9
Andrii Shcherbakov <andrey.shcherbakov05@gmail.com>
2025-09-17 20:36:24 +0300

    Implement uploading .csv file and saving to DB

--- ContactManagerApplication/Controllers/HomeController.cs
+++ ContactManagerApplication/Controllers/HomeController.cs
@@ -1,22 +1,62 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ContactManagerApplication.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ContactManagerApplication.Models;
+using CsvHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactManagerApplication.Controllers
 {
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ContactContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, ContactContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index(CancellationToken cancellationToken)
 		{
-			return View();
+			List<Contact> contacts = await _context.Contacts.ToListAsync(cancellationToken);
+			return View(contacts);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UploadCsv(IFormFile file, CancellationToken cancellationToken)
+		{
+			if (file == null || !file.FileName.EndsWith(".csv") || file.Length == 0)
+			{
+				return BadRequest();
+			}
+
+			try
+			{
+				await using Stream stream = file.OpenReadStream();
+				using StreamReader reader = new StreamReader(stream);
+				using CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+				await _context.Contacts.AddRangeAsync(csv.GetRecords<ContactDto>().Select(ContactDto.ToContactMap), cancellationToken);
+				await _context.SaveChangesAsync(cancellationToken);
+
+				return RedirectToAction(nameof(Index));
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Error processing CSV file");
+				return BadRequest("Error processing CSV file");
+			}
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- ContactManagerApplication/Models/Contact.cs
+++ ContactManagerApplication/Models/Contact.cs
@@ -1,4 +1,5 @@
 using System;
+using CsvHelper.Configuration.Attributes;
 
 namespace ContactManagerApplication.Models
 {
@@ -12,5 +13,36 @@ namespace ContactManagerApplication.Models
 		public decimal Salary { get; set; }
 		public DateTime CreatedDate { get; set; }
 		public DateTime ModifiedDate { get; set; }
+	}
+
+	public class ContactDto
+	{
+		[Name("Name")]
+		public string Name { get; set; }
+
+		[Name("Date of birth")]
+		public DateTime DateOfBirth { get; set; }
+
+		[Name("Married")]
+		public bool IsMarried { get; set; }
+
+		[Name("Phone")]
+		public string Phone { get; set; }
+
+		[Name("Salary")]
+		public decimal Salary { get; set; }
+
+
+		public static Contact ToContactMap(ContactDto contactDto)
+		{
+			return new Contact
+			{
+				Name = contactDto.Name,
+				BirthDate = contactDto.DateOfBirth,
+				IsMarried = contactDto.IsMarried,
+				Phone = contactDto.Phone,
+				Salary = contactDto.Salary
+			};
+		}
 	}
 }
